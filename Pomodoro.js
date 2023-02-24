fa6b8a2f4e0474163da55d0a7e8e884599b469f7
William Isabelle <william.isabelle@etu.univ-grenoble-alpes.fr>
2023-02-24 19:49:45 +0100

    added settings

--- Pomodoro.js
+++ Pomodoro.js
@@ -1,25 +1,27 @@
-var States = ["WORK", "PAUSE", "Doing nothing"];
+var States = ["WORK", "PAUSE", "LPAUSE"];
 class Pomodoro{
     constructor(){
-        this.state = States[2];
+        this.state = States[0];
         this.first_step = true;
     }
 
-    init(workTime, pauseTime, cycles){
+    init(workTime, pauseTime, lpauseTime, cycles){
         this.workTime = workTime;
         this.pauseTime = pauseTime;
-        this.totcycles = cycles;
+        this.lpauseTime = lpauseTime;
+        this.cycles = cycles;
         this.remaining = cycles;
+        this.totcycles = 0;
         this.time = 0;
         this.first_step = true;
-        this.state = States[2];
+        this.state = States[0];
     }
 
     reset(){
         this.first_step = true;
-        this.state = States[2];
+        this.state = States[0];
         this.time = 0;
-        this.remaining = this.totcycles;
+        this.remaining = this.cycles;
     }
 
     time_left(){
@@ -28,9 +30,12 @@ class Pomodoro{
                 return this.workTime - this.time;
                 case States[1]: // IF PAUSING
                 return this.pauseTime - this.time;
-                case States[2]: // IF IDLE
-                return this.workTime - this.time;
+                case States[2]: // IF LONG PAUSING
+                return this.lpauseTime - this.time;
         }
+    }
+    time_left_min(){
+        return this.time_left() / 60;
     }
 
     step(dt){
@@ -42,6 +47,7 @@ class Pomodoro{
             case States[0]:
                 this.time += dt;
                 if(this.time >= this.workTime){
+                    this.totcycles++;
                     this.remaining--;
                     if(this.remaining == 0){
                         this.state = States[2];
@@ -58,9 +64,14 @@ class Pomodoro{
                     this.time = 0;
                 }
                 break;
-            case States[2]: // IF IDLE
+            case States[2]:
+                this.time += dt;
+                if(this.time >= this.lpauseTime){
+                    this.state = States[0];
+                    this.remaining = this.cycles;
+                    this.time = 0;
+                }
                 break;
-
         }
     }
 }
--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 let div_state = document.getElementById("state")
 let div_timer = document.getElementById("timer")
 let div_progress = document.getElementById("progress")
+let div_total = document.getElementById("total")
 
+let workT_settings = document.getElementById("workT-setting")
+let pauseT_settings = document.getElementById("pauseT-setting")
+let cycles_settings = document.getElementById("cycles-setting")
 
 let btn_start = document.getElementById("start")
 let btn_pause = document.getElementById("pause")
 let btn_reset = document.getElementById("reset")
 
-
-let workT = 0.25;
-let pauseT = 0.125;
-let cycles = 1;
-
+defaultUI()
 
 let pause = false
 let loop = null;
@@ -19,9 +19,13 @@ let running = false;
 let pomodoro = new Pomodoro();
 btn_start.onclick = () => {
     if(running) return
+    let workT = min_to_seconds(workT_settings.value);
+    let pauseT = min_to_seconds(pauseT_settings.value);
+    let cycles = cycles_settings.value;
+
     
     running = true;
-    pomodoro.init(workT * 60, pauseT * 60, cycles)
+    pomodoro.init(workT, pauseT, cycles)
     lastT = Date.now() / 1000;
 
     loop = setInterval(function() {
@@ -60,11 +64,35 @@ btn_reset.onclick = () => {
     running = false;
 }
 
+function defaultUI(){
+    div_state.innerHTML = "WORK";
+    div_timer.innerHTML = "25:00";
+    div_progress.innerHTML  = "0/0"
+    div_total.innerHTML = "0"
+}
 
 function updateUI(){
     if(pause) btn_pause.innerText = "Resume";
     if(!pause) btn_pause.innerText = "Pause";
     div_state.innerHTML = pomodoro.state;
-    div_timer.innerHTML = pomodoro.time_left().toString().substring(0, 5);
-    div_progress.innerHTML  = (pomodoro.totcycles - pomodoro.remaining) + "/" + pomodoro.totcycles
+    div_timer.innerHTML = seconds_to_min_str(pomodoro.time_left()).substring(0, 5);
+    console.log(pomodoro.cycles - pomodoro.remaining + "/" + pomodoro.cycles);
+    div_progress.innerHTML  = (pomodoro.cycles - pomodoro.remaining) + "/" + pomodoro.cycles
+    div_total.innerHTML = pomodoro.totcycles
+}
+
+
+function seconds_to_min_str(seconds){
+    let min = Math.floor(seconds / 60);
+    let sec = seconds % 60;
+    return min + ":" + sec;
+}
+
+function min_to_seconds(tmin){
+    return tmin * 60;
+}
+
+function min_str_to_seconds(tmin){
+    let res = tmin.split(":");
+    return res[0] * 60 + res[1];
 }
